308552eedc8e9f6fa08182d6c6b0710c4d1afbbf
DakotaVarnell <jvarnell1@cub.uca.edu>
2023-02-04 23:22:08 -0600

    added files

---
+++ data_loading.py
@@ -0,0 +1,20 @@
+import csv
+# with open('C:\Users\varne\Desktop\Spotify Data\Spotify_final_dataset.csv', newline='') as csvfile:
+#     spamreader = csv.reader(csvfile)
+
+#     for row in range(len(spamreader)-10000):
+#         print(row)
+
+import csv
+
+with open('employee_birthday.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
---
+++ spotify_table.sql
@@ -0,0 +1,11 @@
+create table spotify_data(
+	position NUMBER(38),
+	artist_name VARCHAR2(200),
+	song_name VARCHAR2(200),
+	num_days NUMBER(38),
+	num_times_on_top10 NUMBER(38),
+	peak_position NUMBER(10),
+	peak_streams NUMBER(38),
+	total_streams NUMBER(38));
+	
+	
